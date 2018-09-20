f4ac0019de57e5e144c98d4af903ad55d4ff8a9f
mozzie <mozzie@anniecannons.com>
2018-09-20 11:01:53 -0700

    completed circleCalculations exercise

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -4,7 +4,23 @@
 
 //Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
 
-function circleCalculation () {
-	//your code here
 
-circleCalculation(10);
+
+//Input: radius, a number
+//Output: value of the caluculations (number) OR the sentence (a string)
+
+
+//Add radius a the parameter of our function
+function circleCalculation (r) {
+	//calculcate the area of the circle, store the value in a variable
+	var circleArea = Math.round(Math.PI * Math.pow(r, 2));
+	//calucluate the perimeter, store the value in a variable
+	var circlePerimeter = Math.round(Math.PI * 2 * r);
+	// create a sentence that concatenates the values in the sentance
+	var sentence = ("The area of the circle is " + circleArea + " and the perimeter of the circle is " + circlePerimeter + ".");
+	//return the value of those caluclations OR return a sentence
+	return sentence;
+}
+
+var circleSentence = circleCalculation(10);
+console.log(circleSentence);

0e66d34a346de05cb0fe52f38e1b4bce09c828d0
mozzie <mozzie@anniecannons.com>
2018-10-03 10:57:29 -0700

    Completed dashInsert exercise

--- dashInsert.js
+++ dashInsert.js
@@ -20,11 +20,39 @@ console.log(result) // "44"
 var result = dashInsert("0");
 console.log(result) // "0"
 
-var result = isBigger("13");
+var result = dashInsert("13");
 console.log(result) // "1-3"
 
 **/
 
+
+//Input: A string of numbers
+//Output: A string of numbers with dashes in between any touching odd numbers
+//Purpose: Put dashes in bewteen any touching odd numbers
+
+
 function dashInsert(str) {
-	// Your code
-}
+//Split string into an array of numbers
+//Store the array in a variable
+//Create a variable to store the output of the loop
+//Create a for loop that loops through the elements of the array
+	//Conditional statement that checks if the number we are looking at and the number that comes next in the array are odd (using modulo).
+		// If they are, we want to add the number itself and a dash elements to the varialbe storing our output
+		//If they are not both odd, add the number to the variable storing our output
+		//If numbers next to each other are odd add dash between them using push?
+//Return the variable storing the output from our loop
+	
+	var newString = ""; 
+	var numberArray = str.split("");
+	for (var i = 0; i < numberArray.length; i++) {
+
+		if (numberArray[i] % 2 === 1 && numberArray[i+1] % 2 === 1){
+			newString = newString + numberArray[i] + "-";
+		} else {
+			newString = newString + numberArray[i];
+		}
+	}	
+	return newString;
+	}
+
+

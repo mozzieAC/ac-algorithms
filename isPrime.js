594316d5750b6348edc715713875fda30d4ca3fb
mozzie <mozzie@anniecannons.com>
2018-10-10 11:06:11 -0700

    Completed isPrime exercise

--- isPrime.js
+++ isPrime.js
@@ -31,6 +31,33 @@ console.log(result) // false
 
 **/
 
+//Input: A number between 1 and 2^16
+//Output: return true if the number is prime, return false if it is not.
+//Remember 2 is a prime number and 1 is not.
+
+//if 
+//for loop, i =1; i <= input number; i++
+	//conditional statment: if statement to check if the input number is 1 or 2
+		//if strictly equal to 2 return true
+		//else if strictly equal to 1 return false
+	//checks if the input number is evenly divisible by i with 0 as its remainder, if true return false,
+	//else if input number is strictly equal to 2, return true
+	
+//Hint: You may not need to check numbers up to num to check for primeness.
+
+
+
 function isPrime(num) {
-	// Your code
+	if (num === 1){
+			return false;
+  }
+  for (var i = 2; i < num; i++){
+		 if (num === 2){
+			return true;
+		} else if (num % i === 0) {
+			return false;
+		}
+	}
+  return true;
 }
+
